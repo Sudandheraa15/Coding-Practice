f7c95da37160d41c25deec41e67caab5e62a0120
SUDANDHERAA S V <sudandheraasv@gmail.com>
2026-09-18 17:19:19 +0530

    Add Sort 0s, 1s and 2s (Medium)

---
+++ geeksforgeeks/Medium/Sort 0s, 1s and 2s.go
@@ -0,0 +1,10 @@
+class Solution {
+  public:
+    void sort012(vector<int>& arr) {
+        // code here
+        sort(arr.begin(),arr.end());
+        // for(int i=0;i<arr.size();i++){
+        //     cout<<arr[i]<<" ";
+        // }
+    }
+};
