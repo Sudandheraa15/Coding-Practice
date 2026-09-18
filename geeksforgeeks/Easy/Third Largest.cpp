2af350b98ac339a7fb9216412e9cad6d8ebcc2e7
SUDANDHERAA S V <sudandheraasv@gmail.com>
2026-09-18 14:41:16 +0530

    Add Third Largest (Easy)

---
+++ geeksforgeeks/Easy/Third Largest.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int thirdLargest(vector<int> &arr) {
+        // code here
+        if(arr.size()<3){
+            return -1;
+        }
+        sort(arr.begin(),arr.end());
+        reverse(arr.begin(),arr.end());
+        return arr[2];
+    }
+};
