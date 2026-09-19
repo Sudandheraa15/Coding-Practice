25f102b70257027d4d701e5ac31ca1dcf39e91e9
SUDANDHERAA S V <sudandheraasv@gmail.com>
2026-09-19 14:08:27 +0530

    Add Largest Prime Factor (Medium)

---
+++ geeksforgeeks/Medium/Largest Prime Factor.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    int largestPrimeFactor(int n) {
+        // code here
+       int largest=-1;
+       for(int i=2;i*i<=n;i++){
+           while(n%i==0){
+               largest=i;
+               n/=i;
+           }
+       }
+       if(n>1){
+           largest=n;
+       }
+       return largest;
+    }
+};
